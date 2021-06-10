14077eaef63eabf21f047b3f0e38affc082e27f2
Mariana Licona <m.licona@abtis.de>
2021-06-10 14:20:55 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FacturacionTiendaDePeluches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenid@ a Peluchilandia");
+            Console.WriteLine("Sistema de Facturacion Peluchilandia");
+            Console.WriteLine("Sigue los pasos indicados para crear tu Peluchi-Factura");
+            Console.WriteLine("Ingresa el numero de piezas");
+            Console.WriteLine("Describe tu producto");
+            Console.WriteLine("Indica cuantas piezas vas a ordenar:");
+            Console.WriteLine("Indica el precio:");
+            Console.WriteLine("Tu total a pagar es de:" + "MXN");
+        }
+    }
+}
